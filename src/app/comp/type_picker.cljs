bc011b0c86c4883cffc7b9d1c44b3f8e593c605d
jiyinyiyong <jiyinyiyong@gmail.com>
2019-02-21 00:45:38 +0800

    some better UI displaying node types

--- src/app/comp/type_picker.cljs
+++ src/app/comp/type_picker.cljs
@@ -3,23 +3,44 @@
   (:require [hsl.core :refer [hsl]]
             [respo-ui.core :as ui]
             [respo.comp.space :refer [=<]]
-            [respo.core :refer [defcomp cursor-> <> action-> span div]]
+            [respo.core :refer [defcomp cursor-> list-> <> action-> span div]]
             [app.config :as config]
             [respo-alerts.comp.alerts :refer [comp-select]]
-            [app.style :as style]))
+            [app.style :as style]
+            [inflow-popup.comp.popup :refer [comp-popup]]))
+
+(defcomp
+ comp-node-type
+ (x on-pick)
+ (div
+  {:style {:padding "0 12px",
+           :cursor :pointer,
+           :background-color (hsl 0 0 96),
+           :line-height "32px",
+           :margin-bottom 8},
+   :on-click (fn [e d! m!] (on-pick x d! m!))}
+  (<> (:display x))))
+
+(defn find-option [x options]
+  (if (empty? options)
+    nil
+    (let [x0 (first options)] (if (= x (:value x0)) x0 (recur x (rest options))))))
 
 (def node-types
-  [{:value :box, :display "Box"}
-   {:value :space, :display "Space"}
-   {:value :button, :display "Button"}
-   {:value :link, :display "Link"}
-   {:value :icon, :display "Icon"}
-   {:value :text, :display "Text"}
-   {:value :some, :display "Some"}
-   {:value :template, :display "Template"}
-   {:value :list, :display "List"}
-   {:value :slot, :display "Slot"}
-   {:value :input, :display "Input"}])
+  [{:value :box, :kind :layout, :display "Box"}
+   {:value :space, :kind :layout, :display "Space"}
+   {:value :button, :kind :element, :display "Button"}
+   {:value :link, :kind :element, :display "Link"}
+   {:value :icon, :kind :element, :display "Icon"}
+   {:value :text, :kind :element, :display "Text"}
+   {:value :input, :kind :element, :display "Input"}
+   {:value :some, :kind :control, :display "Some"}
+   {:value :template, :kind :control, :display "Template"}
+   {:value :list, :kind :control, :display "List"}
+   {:value :slot, :kind :control, :display "Slot"}])
+
+(defn render-title [title]
+  (div {:style {:font-family ui/font-fancy, :color (hsl 0 0 70), :margin-top 20}} (<> title)))
 
 (defcomp
  comp-type-picker
@@ -29,12 +50,46 @@
   (<> "Node Type:" style/field-label)
   (=< 8 nil)
   (cursor->
-   :type
-   comp-select
+   :popup
+   comp-popup
    states
-   (:type markup)
-   node-types
-   {}
-   (fn [result d! m!]
-     (if (some? result)
-       (d! :template/node-type {:template-id template-id, :path focused-path, :type result}))))))
+   {:trigger (<>
+              (let [v (find-option (:type markup) node-types)]
+                (if (nil? v) "Nothing" (:display v))))}
+   (fn [on-toggle]
+     (let [on-pick (fn [result d! m!]
+                     (d!
+                      :template/node-type
+                      {:template-id template-id, :path focused-path, :type (:value result)})
+                     (on-toggle m!))]
+       (div
+        {:style (merge ui/row {:width 400})}
+        (div
+         {:style ui/flex}
+         (render-title "Layout")
+         (list->
+          {}
+          (->> node-types
+               (filter (fn [x] (= :layout (:kind x))))
+               (map (fn [x] [(:value x) (comp-node-type x on-pick)]))))
+         (render-title "Elements")
+         (list->
+          {}
+          (->> node-types
+               (filter (fn [x] (= :element (:kind x))))
+               (map (fn [x] [(:value x) (comp-node-type x on-pick)])))))
+        (=< 16 nil)
+        (div
+         {:style ui/flex}
+         (render-title "Controls")
+         (list->
+          {}
+          (->> node-types
+               (filter (fn [x] (= :control (:kind x))))
+               (map (fn [x] [(:value x) (comp-node-type x on-pick)]))))
+         (render-title "Other")
+         (list->
+          {}
+          (->> node-types
+               (filter (fn [x] (= :other (:kind x))))
+               (map (fn [x] [(:value x) (comp-node-type x on-pick)])))))))))))
