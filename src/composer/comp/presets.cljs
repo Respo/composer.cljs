fe80664850ffd6d20516608ac272864491a33b67
jiyinyiyong <jiyinyiyong@gmail.com>
2019-04-07 18:41:04 +0800

    change picker layout for presets

--- src/composer/comp/presets.cljs
+++ src/composer/comp/presets.cljs
@@ -11,61 +11,67 @@
             [composer.style :as style]))
 
 (def builtin-presets
-  #{:expand :font-code :font-fancy :font-normal :fullscreen :scroll :global :base-padding})
+  {"Fonts" [:font-code :font-fancy :font-normal],
+   "Layouts" [:expand :fullscreen :base-padding],
+   "Features" [:scroll :global]})
 
 (defcomp
  comp-preset
- (preset on-click)
+ (preset selected? on-click)
  (div
-  {:style {:padding "0 10px",
-           :margin 2,
-           :cursor :pointer,
-           :background-color (hsl 200 80 60),
-           :color :white,
-           :border-radius "16px",
-           :line-height "24px"},
+  {:style (merge
+           {:padding "0 10px",
+            :margin 2,
+            :cursor :pointer,
+            :background-color (hsl 200 80 84),
+            :color :white,
+            :border-radius "4px",
+            :line-height "24px"}
+           (if selected? {:background-color (hsl 200 80 60)})),
    :on-click on-click}
   (<> (name preset))))
 
 (defcomp
  comp-presets-picker
  (states presets template-id path)
- (cursor->
-  :picker
-  comp-popup
-  states
-  {:trigger (comp-i :edit 14 (hsl 200 80 50)), :style {:display :inline-block}}
-  (fn [toggle!]
-    (div
-     {}
-     (<> "Selected")
-     (if (empty? presets)
-       (div {} (<> "Nothing selected" {:color (hsl 0 0 80)}))
-       (list->
-        {:style ui/row}
-        (->> presets
-             (map
-              (fn [preset]
-                [preset
-                 (comp-preset
-                  preset
-                  (fn [e d! m!]
-                    (d!
-                     :template/node-preset
-                     {:template-id template-id, :path path, :op :remove, :value preset})))])))))
-     (<> "Available")
-     (list->
-      {:style ui/row}
-      (->> (difference builtin-presets presets)
-           (map
-            (fn [preset]
-              [preset
-               (comp-preset
-                preset
-                (fn [e d! m!]
-                  (d!
-                   :template/node-preset
-                   {:template-id template-id, :path path, :op :add, :value preset})))]))))))))
+ (let [handle-op (fn [op preset d!]
+                   (d!
+                    :template/node-preset
+                    {:template-id template-id, :path path, :op op, :value preset}))]
+   (cursor->
+    :picker
+    comp-popup
+    states
+    {:trigger (comp-i :edit 14 (hsl 200 80 50)), :style {:display :inline-block}}
+    (fn [toggle!]
+      (list->
+       {}
+       (->> builtin-presets
+            (map
+             (fn [[group-name xs]]
+               [group-name
+                (div
+                 {:style ui/column}
+                 (=< nil 16)
+                 (div
+                  {}
+                  (<>
+                   group-name
+                   {:font-family ui/font-fancy, :font-size 18, :font-weight 300}))
+                 (list->
+                  {:style ui/row}
+                  (->> xs
+                       (map
+                        (fn [preset]
+                          [preset
+                           (let [selected? (contains? (set presets) preset)]
+                             (comp-preset
+                              preset
+                              selected?
+                              (fn [e d! m!]
+                                (if selected?
+                                  (handle-op :remove preset d!)
+                                  (handle-op :add preset d!)))))])))))]))))))))
 
 (defcomp
  comp-presets
@@ -85,6 +91,7 @@
            [preset
             (comp-preset
              preset
+             false
              (fn [e d! m!]
                (d!
                 :template/node-preset
